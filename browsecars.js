9123f6c4bf7e19245beebc387b90d0f552779941
priyabaskaran-11b <priyabaskaran.11b@gmail.com>
2026-05-25 17:35:04 +0530

    removed loader

--- browsecars.js
+++ browsecars.js
@@ -628,20 +628,7 @@ closeFilter.addEventListener("click", () => {
 });
 
 
-/* =========================
-   LOADER
-========================= */
 
-window.addEventListener("load", () => {
-
-    setTimeout(() => {
-
-        document.getElementById("loader")
-            .style.display = "none";
-
-    }, 1200);
-
-});
 
 
 /* =========================
